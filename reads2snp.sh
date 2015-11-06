fa180e03d3fb77fcd4de1b025dac06bb6b346608
Tessa Pierce <ntpierce@ucsd.edu>
2015-11-06 14:30:17 -0800

    minor updates

--- reads2snp.sh
+++ reads2snp.sh
@@ -10,11 +10,12 @@
 #-bqt 20
 #-rqt 20
 
-
 ./reads2snp_2.0.64.bin -bam ${1} -bamref ${2} -min 30 -nbth 16 -spa -bqt 20 -rqt 20 & # coverage 30
 
-## just checking:
+# other coverages
 ###./reads2snp_2.0.64.bin -bam ${1} -bamref ${2} -min 1 -nbth 8 -spa -bqt 20 -rqt 20 & # coverage 1
+###./reads2snp_2.0.64.bin -bam ${1} -bamref ${2} -min 20 -nbth 8 -spa -bqt 20 -rqt 20 & # coverage 20
+###./reads2snp_2.0.64.bin -bam ${1} -bamref ${2} -min 40 -nbth 8 -spa -bqt 20 -rqt 20 & # coverage 40
 
 
 
